56794ddcefae3428337703872f2dda877b9acfd6
Staskkk <MblJlOCTACA@mail.ru>
2014-06-22 18:42:30 +0300

    Create stackpos.lua

---
+++ Libraries/stackpos.lua
@@ -0,0 +1,20 @@
+-- Made my Staskkk.
+-- Save as stackpos.lua and place this script to the Ensage\Scripts\libs folder.
+-- 1: pull point, 2: fountain, 3: wait point 4: team
+routes = {
+
+	{Vector(-1131,-4044,127), Vector(-3584,-3475,128), Vector(-1512,-3661,127), team = 2},
+	{Vector(-366,-2945,127), Vector(-576,-1192,127), Vector(-576,-1192,127), team = 2},
+	{Vector(1617,-3722,256), Vector(1691,-5788,256), Vector(1610,-4197,256), team = 2},
+	{Vector(3126,-3439,256), Vector(5202,-4461,256), Vector(3329,-3886,256), team = 2},
+	{Vector(3044,-4760,256), Vector(5178,-4443,256), Vector(3107,-5137,256), team = 2},
+	{Vector(-2822,151,256), Vector(-3483,-1735,247), Vector(-2433,-356,256), team = 2},
+
+	{Vector(1167,3295,256), Vector(345,5274,256), Vector(1011,3656,256), team = 3},
+	{Vector(-371,3768,256), Vector(-600,5428,256), Vector(-515,4845,256), team = 3},
+	{Vector(-1588,2697,127), Vector(-1308,4895,256), Vector(-1491,2986,127), team = 3},
+	{Vector(-3116,4668,256), Vector(-658,5016,256), Vector(-3086,4924,256), team = 3},
+	{Vector(-4382,3612,256), Vector(-1995,3516,27), Vector(-3995,3984,256), team = 3},
+	{Vector(4447,-1950,127), Vector(5480,-282,256), Vector(4697,-1377,127), team = 3}
+
+}
